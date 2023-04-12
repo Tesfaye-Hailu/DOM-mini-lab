7ef660b0ce6323bba90769a346acdc5e79be5edb
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-04-12 16:01:41 -0700

    second commit

---
+++ dom1.js
@@ -0,0 +1,27 @@
+const h1 = document.querySelector('h1');
+h1.textContent = 'Hello PerScholas SEA';
+
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  // Code to be executed when the button is clicked
+});
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  const h1 = document.querySelector('h1');
+  h1.style.display = 'none';
+});
+
+
+const unhideButton = document.querySelector('#unhide-btn');
+unhideButton.addEventListener('click', function() {
+  // Code to be executed when the "unhide" button is clicked
+});
+
+
+const unhideButton = document.querySelector('#unhide-btn');
+unhideButton.addEventListener('click', function() {
+  const h1 = document.querySelector('h1');
+  h1.style.display = 'block';
+});
